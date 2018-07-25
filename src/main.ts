994cc91570685d5e6bec0b6fa6071087ae821bb0
jdu <jdu.xian@gmail.com>
2018-07-25 09:53:16 +0800

    use rsvp to create promise for observable

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,30 @@
 import { Observable } from "rxjs";
 import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/observable/fromPromise';
+import { br, log } from "./util";
+import RSVP from "rsvp";
 
 Observable.of(1, 2, 3)
-  .subscribe(x => document.writeln(x.toString()));
+  .subscribe(x => log(x.toString ()));
 
+br();
+
+Observable.from([1, 2, 3])
+  .subscribe(x => log(x.toString ()));
+
+br();
+
+Observable.fromEvent(document.querySelector('button'), 'click')
+  .subscribe(x => alert('clicked'));
+
+Observable.fromPromise(fetch('http://localhost:3000/data').then(x =>  x.json()))
+  .subscribe(response => {
+    console.log(response.a);
+  });
+
+Observable.fromPromise(new RSVP.Promise(( resolve ) => resolve ({a: 1})))
+  .subscribe(response => {
+    console.log(response);
+  });
---
+++ src/util.ts
@@ -0,0 +1,7 @@
+export function log ( x: any ): void {
+  document.writeln(x.toString());
+}
+
+export function br () {
+  document.write('<br>');
+}
